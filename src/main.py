10a23a9ee69854873e491200de0b0f3fd10a2ed8
petmik2018 <petmik@yandex.ru>
2021-10-30 21:43:48 +0300

    Lesson 3 HW completed

---
+++ src/main.py
@@ -0,0 +1,18 @@
+abc = Triangle(3, 4, 5)
+
+abc.perimeter
+abc.area
+# klmn = Rectangle(3, 5)
+# klmn.area
+#
+# opqr = Square(3)
+# opqr.area
+
+# print(opqr + klmn)
+
+ooo = Circle(2)
+ooo.area
+
+print(ooo + abc)
+
+
